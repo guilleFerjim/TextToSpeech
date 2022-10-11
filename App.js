8899daffe6edb3503b49eaeb84a52cfa0720fe97
guilleFerjim <g.fernandezj.2019@alumnos.urjc.es>
2022-10-11 09:16:25 +0300

    dòne

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { View, StyleSheet, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  
+  const [text, setText] = useState('');
+  
+  const speak = () => {
+    Speech.speak(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={{width: 100, height: 40, borderColor: 'gray', borderWidth: 1,}} onChangeText={(text) => setText(text) }/>
+      <Button title="Press to hear" onPress={speak} />
     </View>
   );
 }
